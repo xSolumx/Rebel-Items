89b8f8c5f15f2decd96f02735d41067255375236
Solum <haydaust@gmail.com>
2021-07-25 16:36:59 +0200

    Added code to vampire sword Added utilities class to change the colour depending on the value given Updated vamp sword config with negative min value

--- src/main/java/com/mcrebels/rebelitems/rebelitems/Utilities.java
+++ src/main/java/com/mcrebels/rebelitems/rebelitems/Utilities.java
@@ -1,6 +1,10 @@
 package com.mcrebels.rebelitems.rebelitems;
 
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.entity.Player;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 
 public class Utilities {
@@ -13,4 +17,49 @@ public class Utilities {
         }
         return false;
     }
+
+    public static String getWorthColour(double min, double max, double num){
+        DecimalFormat fmt = new DecimalFormat("#.#");
+        fmt.setRoundingMode(RoundingMode.HALF_UP);
+        double percent = ((num-min)/(max-min))*100;
+        num = Double.parseDouble(fmt.format(num*100));
+        //#ff0101 #df3e3e #b96565 #875d5d //#555555// #577a57 #5a9f5a #4fc74c #15ff019
+
+        String toReturn = ""+num;
+        if (percent == 100){
+            toReturn = "<#ffc400>"+num;
+        }
+        else if (percent == 0){
+            toReturn = "<#521717>"+num;
+        }
+        else if (percent >= 90){
+            toReturn = "<#15ff019>"+num;
+        }
+        else if (percent >= 75){
+            toReturn = "<#4fc74c>"+num;
+        }
+        else if (percent >= 60){
+            toReturn = "<#5a9f5a>"+num;
+        }
+        else if (percent > 50){
+            toReturn = "<#577a57>"+num;
+        }
+        else if (percent == 50){
+            toReturn = "<#555555>"+num;
+        }
+        else if (percent < 50){
+            toReturn = "<#875d5d>"+num;
+        }
+        else if (percent < 40){
+            toReturn = "<#b96565>"+num;
+        }
+        else if (percent < 25){
+            toReturn = "<#df3e3e>"+num;
+        }
+        else if (percent < 10){
+            toReturn = "<#ff0101>"+num;
+        }
+        return toReturn;
+    }
+
 }
--- src/main/java/com/mcrebels/rebelitems/rebelitems/allItems/swords/vampireSword.java
+++ src/main/java/com/mcrebels/rebelitems/rebelitems/allItems/swords/vampireSword.java
@@ -4,6 +4,8 @@ import com.mcrebels.rebelitems.rebelitems.allItems.Item;
 import com.mcrebels.rebelitems.rebelitems.RebelItems;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.md_5.bungee.api.ChatColor;
+import org.apache.commons.lang.math.RandomUtils;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
@@ -15,10 +17,16 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
+import static com.mcrebels.rebelitems.rebelitems.Utilities.getWorthColour;
 import static com.mcrebels.rebelitems.rebelitems.Utilities.metaCheck;
+import static org.apache.commons.lang.math.RandomUtils.nextDouble;
+import static org.bukkit.Bukkit.getServer;
 
 public class vampireSword extends Item implements Listener {
 
@@ -31,8 +39,8 @@ public class vampireSword extends Item implements Listener {
     private final Material itemMaterial = Material.NETHERITE_SWORD;
     private final NamespacedKey leechPercentKey = new NamespacedKey(plugin, "leechPercent");
     private List<Component> itemLore;
-    private static double minPercent = .02;
-    private static double maxPercent = .1;
+    private static double minPercent = -0.05;
+    private static double maxPercent = 0.1;
     private double percent;
 
 
@@ -54,11 +62,19 @@ public class vampireSword extends Item implements Listener {
         item = new ItemStack(itemMaterial, 1);
         percent = (Math.random() * (maxPercent - minPercent) + minPercent);
         percent = Math.floor(percent * 1000) / 1000;
+
+        //useless code for bug testing aand shit
+        DecimalFormat fmt = new DecimalFormat("#.#");
+        fmt.setRoundingMode(RoundingMode.HALF_UP);
+        getServer().getConsoleSender().sendMessage(""+percent);
+        getServer().getConsoleSender().sendMessage(fmt.format(percent*100));
+        //ends here
+
         itemLore = Arrays.asList(
                 MiniMessage.markdown().parse("<gradient:green:blue>===================</gradient>"),
                 MiniMessage.markdown().parse("<gradient:green:blue>Grants the user a small amount of </gradient>"),
                 MiniMessage.markdown().parse("<gradient:green:blue>damage dealt as health</gradient>"),
-                MiniMessage.markdown().parse("<gradient:green:blue>Leech amount: " + percent * 100 + "% of damage</gradient>"));
+                MiniMessage.markdown().parse("<blue>Leech amount: " + getWorthColour(minPercent, maxPercent, percent)));
         ItemMeta tMeta = item.getItemMeta();
         tMeta.getPersistentDataContainer().set(leechPercentKey, PersistentDataType.DOUBLE, percent);
         tMeta.setCustomModelData(customMetaID);
